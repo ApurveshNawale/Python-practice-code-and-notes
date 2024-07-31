b6485892f594c3bb7a5da5c393f7679374043b8f
ApurveshNawale <apurveshnawale0332@gmail.com>
2024-07-31 18:53:49 +0530

    python practice

---
+++ python code/chapter1/chapter 2/4_practice_String.py
@@ -0,0 +1,10 @@
+### WAP to input user’s first name & print its length
+
+#n = input("Enter your name : ")
+#print(len(n))
+
+
+### WAP to find the occurrence of ‘$’ in a String. 
+
+m = str(input(" Enter $ for count : "))
+print(str.count(m,"$"))
---
+++ python code/chapter1/chapter 2/6_practice_conditonal_statement.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age : "))
+if age >= 18 and age <= 50 :
+    print("Your are eligibal for driving Licence ")
+elif age <= 17 :
+    print("Your are not eligibal for driving Licence ")
+else :
+    print("You are over age for driving licence ")
