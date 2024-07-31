8da924d39ec9461792705a2ebd4836489d244c64
ApurveshNawale <apurveshnawale0332@gmail.com>
2024-07-31 19:24:13 +0530

    practice code of if_else condition

--- python code/chapter1/chapter 2/9_prctice_if-else.py
+++ python code/chapter1/chapter 2/9_prctice_if-else.py
@@ -1,24 +1,26 @@
-# # ## WAP to check if a number entered by the user is odd or even
+## WAP to check if a number entered by the user is odd or even
 
-# n = int(input("Enter your No : "))
-# rem = n % 2
-# if (rem == 0):
-#     print("the number is even Number")
-# else :
-#     print("The number is odd number")
+n = int(input("Enter your No : "))
+rem = n % 2
+if (rem == 0):
+    print("the number is even Number")
+else :
+    print("The number is odd number")
 
 
 # ## WAP to find the greatest of 3 numbers entered by the user
 
-# a = int(input("Enter First number : "))
-# b = int(input("Enter seconed number : "))
-# c = int(input("Enter third number : "))
-# if a > b :
-#     print("The  First number",a, " is gratest number")
-# elif a < b and b > c :
-#     print("The seconed number",b,"is gratest number")
-# else :
-#     print("The third number",c, "is gratest number")
+a = int(input("Enter First number : "))
+b = int(input("Enter seconed number : "))
+c = int(input("Enter third number : "))
+if a > b :
+    print("The  First number",a, " is gratest number")
+elif a < b and b > c :
+    print("The seconed number",b,"is gratest number")
+else :
+    print("The third number",c, "is gratest number")
+
+
 
 ## WAP to check if a number is a multiple of 7 or not
 
