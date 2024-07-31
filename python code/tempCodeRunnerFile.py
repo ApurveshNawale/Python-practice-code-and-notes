06c333efe02fbd3a43c614d38ad52eef00842438
ApurveshNawale <apurveshnawale0332@gmail.com>
2024-07-31 14:18:53 +0530

    first commit

--- python code/tempCodeRunnerFile.py
+++ python code/tempCodeRunnerFile.py
@@ -1,9 +1,2 @@
-i = 1
-while i <= 4 :
-    print(i * "*")
-    i = i + 1
-temperature = 25.5
-kelvin = 273
-sum = (temperature + kelvin)
-print("Celsius -",temperature)
-print("Kelvin -",sum)
+
+    print("False")
