854c73a2ed6868bde6d1de2215771c47fcea2a32
ApurveshNawale <apurveshnawale0332@gmail.com>
2024-07-31 14:20:16 +0530

    commit

--- python code/PRACTICE1.py
+++ python code/PRACTICE1.py
@@ -1,20 +1,22 @@
 ## Write a Program to input 2 numbers & print their sum
 
-#a = int(input("Enter 1st no : "))
-#b = int(input("Enter 2nd no : "))
-#sum = a + b
-#print("sum :" , sum)
+a = int(input("Enter 1st no : "))
+b = int(input("Enter 2nd no : "))
+sum = a + b
+print("sum :" , sum)
 
 ### WAP to input side of a square & print its area
-#side = int(input("Enter square side :"))
-#area = side * side
-# print("Area :", area)
+
+side = int(input("Enter square side :"))
+area = side * side
+print("Area :", area)
 
 ### WAP to input 2 floating point numbers & print their average.
-#first = float(input("Enter 1st no : "))
-#second = float(input("Enter 2nd no :"))
-#avarage = (first + second) / 2
-#print("Avarage :" , avarage)
+
+first = float(input("Enter 1st no : "))
+second = float(input("Enter 2nd no :"))
+avarage = (first + second) / 2
+print("Avarage :" , avarage)
 
 
 
