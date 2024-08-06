3bf75fa046c5c77bd1de565f30ae31d3586b6fe7
ApurveshNawale <apurveshnawale0332@gmail.com>
2024-08-06 16:16:00 +0530

    list practice

---
+++ python code/chapter1/chapter 2/chapter_3/5_practice.py
@@ -0,0 +1,22 @@
+# list = []
+# list1 = input("Enter : ")
+# list.append(list1)
+
+# copy_list = list.copy()
+# copy_list.reverse()
+
+# if (copy_list == list):
+#     print("palindrome")
+# else :
+#     print("NOt palindrome")
+
+
+list = [1,2,3]
+
+copy_list = list.copy()
+copy_list.reverse()
+
+if (copy_list == list):
+    print("plaindrome")
+else:
+    print("Not palindrome")
---
+++ python code/chapter1/chapter 2/p.py
@@ -0,0 +1,13 @@
+# cook your dish here
+n = int(input("enter :"))
+if n >= 90:
+    print("A")
+    
+elif n >= 70 and n <= 80 :
+    print("B")
+
+elif n >=30 and n <= 40 :
+    print("C")
+    
+else:
+    print("F")
